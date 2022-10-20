7b81a0715b7df3675a7e833a0c3ebdccecd326d7
fria-li <nfria5415@gmail.com>
2022-10-20 02:00:49 +0200

    updated the main

--- main.cpp
+++ main.cpp
@@ -3,23 +3,20 @@
 
 int main()
 {
-    system("CLS");
     BigDecimalInt num1("+123456789012345678901234567890");
     BigDecimalInt num2("+113456789011345678901134567890");
-
-    BigDecimalInt num3 = num2 - num1;
-    BigDecimalInt num4 = num1 < num2;
-    
-
+    BigDecimalInt num3("-200000000000000000000000000000");
+    BigDecimalInt num4 = num2 + num1;
+    BigDecimalInt num5 = num2 - num1;
+    BigDecimalInt num6 = num1 < num2;
     cout << "num1 = " << num1 << endl;
-
     cout << "num2 = " << num2 << endl;
+    cout << "num3 = " << num3 << endl;
+    cout << "num2 + num1 = " << num4 << endl;
+    cout << "num2 - num1 = " << num5 << endl;
+    cout << "num1 < num2 = " << num6 << endl;
 
-    cout << "num2 - num1 = " << num3 << endl;
-
-    cout << "num1 < num2 = " << num4 << endl;
-    
 
 
-   
+
 }
